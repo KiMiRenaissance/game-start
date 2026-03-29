9af0e8244926e7d7b6ea8433fc469f8045343ac8
KiMiRe <milanoinrock01@gmail.com>
2026-03-29 18:39:47 +0700

    Refactor bot responses and adjust button styles

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const pertanyaan = document.getElementById("pertanyaan")
 const jawaban = document.getElementById("jawaban")
 const containerJawaban = document.getElementById("row")
+const kembali = document.getElementById("btnKembali")
 
 let init = 0
 
@@ -11,7 +12,7 @@ const botSay = (data) => {
         `Hehehe... kamu tahu? aku selalu iri mengetahui manusia bisa bebas berekspresi, sedangkan aku hanya program yang dirangcang oleh mereka. Menurutmu apa yang bisa menghiburku?`,
         `aku rasa menggambar menyenangkan tapi... ayo kita bermain game. bukankah game opsi yang terbaik untuk meninggalkan pekerjaan kita?`,
         `Ah bercanda, pekerjaan merupakan hal wajib untuk kita yang sedang di usia produktif, jadi gimana kita akan bermain game? Apakah di usiamu saat ini masih layak untuk main game? Berapa usiamu sekarang?`,
-        `aku rasa usia ${data?.usia} tidak begitu buruk. 😉`,
+        `aku rasa berusia ${data?.usia} tahun tidak begitu buruk. 😉`,
     ]
 }
 
@@ -24,13 +25,17 @@ function botStart() {
     init++
     if (init === 1) {
         botDelay({ nama: jawaban.value })
+        btnKembali.style.display = "none";
             setTimeout(() => {
                 containerJawaban.style.display = 'none';
                 btnKirim.innerHTML = 'ya, aku suka mendengar namaku.';
                 userData.push(jawaban.value)
                 jawaban.value = "ya, aku suka mendengar namaku."
                 btnKirim.style.fontSize = ".6vh"
-            }, [1750])
+                btnKirim.style.width = "auto";
+                btnKirim.style.height = "auto";
+                btnKirim.style.padding = "5px";
+            }, [1500])
     } else if (init === 2) {
         botDelay({ canggung: jawaban.value })
             setTimeout(() => {
@@ -38,9 +43,8 @@ function botStart() {
                 userData.push(jawaban.value)
                 jawaban.value = "Mungkin kegiatan menyenangkan seperti, menggambar."
                 btnKirim.innerHTML = 'Mungkin kegiatan menyenangkan seperti, menggambar.';
-                btnKirim.style.fontSize = ".6vh"
-                btnKirim.style.width = "8vh"
-            }, [1750])
+                btnKirim.style.fontSize = ".6vh";
+            }, [1500])
     } else if (init === 3) {
         botDelay({ nyocot: jawaban.value })
             setTimeout(() => {
@@ -49,28 +53,44 @@ function botStart() {
                 userData.push(jawaban.value)
                 jawaban.value = "Tunggu, apa maksudmu itu!?"
                 btnKirim.style.fontSize = ".6vh"
-            }, [1750])
+            }, [1500])
     } else if (init === 4) {
         botDelay({ gitulah: jawaban.value })
             setTimeout(() => {
                 containerJawaban.style.display = 'block';
                 jawaban.placeholder = "tulis usiamu di sini!";
-                btnKirim.innerHTML = 'send';
+                jawaban.type = 'number';
+                btnKirim.innerHTML = 'ini usiaku';
                 userData.push(jawaban.value)
                 jawaban.value = ""
                 btnKirim.style.fontSize = "1.4vh"
-            }, [1750])
+            }, [1500])
+    } else if (jawaban.value <= 15) {
+        containerJawaban.style.display = "none";
+        jawaban.style.display = "none";
+        jawaban.value = "1000";
+        alert(`Aku ingin ${userData[0]} tidak memainkan permainan ini.`)
+        pertanyaan.innerHTML = `Maaf ${userData[0]}, aku tidak bisa bermain denganmu.`
+        btnKirim.innerHTML = "tetap ikut";
+        btnKembali.style.display = "block";
+        btnKembali.addEventListener("click", function() {
+            window.location.href = 'https://kimirenai.vercel.app'
+        });
+        jawaban.type = 'text';
     } else if (init === 5) {
         botDelay({ usia: jawaban.value })
+        btnKembali.style.display = "none";
             setTimeout(() => {
                 containerJawaban.style.display = 'none';
                 btnKirim.innerHTML = 'Okie dokie...';
                 userData.push(jawaban.value)
-                jawaban.value = "Okie dokie..."
-                btnKirim.style.fontSize = ".8vh"
-            }, [1750])
+                jawaban.type = 'text';
+                jawaban.value = "Okie dokie...";
+                btnKirim.style.fontSize = ".8vh";
+            }, [1500])
     } else if (init === 6) {
         finishing({ nya: jawaban.value })
+        btnKembali.style.display = "none";
     } else {
         botEnd()
     }
@@ -78,9 +98,13 @@ function botStart() {
 
 function botDelay(jawabanUser) {
     console.log({ userData: userData })
+    btnKirim.style.display = 'none';
+    jawaban.style.display = 'none';
     setTimeout(() => {
         pertanyaan.innerHTML = botSay(jawabanUser)[init]
-    }, [1750])
+        jawaban.style.display = 'block';
+        btnKirim.style.display = 'block';
+    }, [1500])
     userData.push(jawaban.value)
     jawaban.value = ""
 }
@@ -97,9 +121,8 @@ function finishing() {
 }
 
 function botEnd() {
-    setTimeout(() => {
-        window.location.href = "https://asyukkabe.vercel.app";
-    }, [2500])
+    alert(`Ketika ${userData[0]} merasa tidak nyaman dalam permainan, harap tidak dilanjutkan.`)
+    window.location.href = "https://asyukkabe.vercel.app";
 }
 
 const karakter = document.getElementById('karakter');
@@ -116,7 +139,7 @@ tombol.addEventListener('click', function() {
 
     setTimeout(function() {
         karakter.src = "ngapainjugadiklik.gif";
-    }, 1250);
+    }, 1400);
 });
 
 document.getElementById("jawaban").addEventListener("keypress", function(e) {
